f20c9871b4342011b1984b4c84f81d39a8eba101
James-S-B <jamessbenerofe23@gmail.com>
2022-03-20 17:02:31 -0700

    init rps

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 'strict'
 
-function plus7(number) {
-    return number + 7;
-}
+let randomNumber = Number;
+let guess = String;
 
-console.log(plus7(3));
+function computerPlay() {
+
+
+    randomNumber = Math.floor(1 + Math.random() * (2 - 0 + 1));
+
+    if (randomNumber = 1) {
+        guess = "rock"
+    }
+    if (randomNumber = 2) {
+        guess = "paper"
+    }
+    if (randomNumber = 3) {
+        guess = "sizors"
+    }
+    return guess
+}
+console.log(computerPlay())
